14df07a903dba9a7a513edf4f160f0ddd8c2e0a8
Trigs70 <98355804+Trigs70@users.noreply.github.com>
2024-02-09 21:18:26 +0200

    new

---
+++ foto/info.js
@@ -0,0 +1,6 @@
+const div = document.createElement('div');
+const a = document.createElement ('a');
+a.href = 'https://books.goalkicker.com/';
+a.innerText = 'books';
+document.body.appendChild (a);
+div.appendChild (a);
